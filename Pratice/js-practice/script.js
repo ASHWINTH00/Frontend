cb5f4ce72fe98b1d6de5879137c5239f03b74523
Ashwinth <ashwinth112plus@gmail.com>
2025-06-24 11:17:00 +0530

    as

---
+++ Pratice/js-practice/script.js
@@ -0,0 +1,13 @@
+
+ function sub(){
+    let font=document.querySelector('.js-btn');
+  if(font.innerHTML ==="Subscribe"){
+    font.innerHTML="Subscribed"
+    fontElement.innerText = "Subscribed";
+
+  }
+  else{
+    font.innerHTML="Subscribe"
+  
+  } 
+ }
